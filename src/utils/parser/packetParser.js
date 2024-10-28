8c8e8a2a09ff6558d60047e9ee3a3d96547feb46
hanwj75 <wjws9608@gmail.com>
2024-10-29 03:38:10 +0900

    Update "오타수정"

--- src/utils/parser/packetParser.js
+++ src/utils/parser/packetParser.js
@@ -35,26 +35,24 @@ export const packetParser = (data) => {
   }
 
   const [namespace, typeName] = protoTypeName.split("."); //handlers/index.js 에 있는 initial.InitialPacket 을 .을 기준으로 나눈거임
-  const PayloacType = protoMessages[namespace][typeName];
+  const PayloadType = protoMessages[namespace][typeName];
   let payload;
 
   try {
-    payload = PayloacType.decode(packet.payload);
+    payload = PayloadType.decode(packet.payload);
   } catch (err) {
     throw new CustomError(ErrorCodes.PACKET_DECODE_ERROR, `패킷디코딩 오류임`);
   }
 
-  const errorMessage = PayloacType.verify(payload);
+  const errorMessage = PayloadType.verify(payload);
   if (errorMessage) {
     throw new CustomError(ErrorCodes.INVALID_PACKET, `패킷 구조 이상함${errorMessage}`);
   }
 
   //필드가 비어있는 경우 = 필수 필드가 누락된 경우
-  const expectedFields = Object.keys(PayloacType.fields);
+  const expectedFields = Object.keys(PayloadType.fields);
   const actualFields = Object.keys(payload);
-  const missingFields = expectedFields.filter((field) => {
-    !actualFields.includes(field);
-  });
+  const missingFields = expectedFields.filter((field) => !actualFields.includes(field));
   if (missingFields.length > 0) {
     throw new CustomError(ErrorCodes.MISSING_FIELDS, `필수 필드 누락 ${missingFields.join(",")}`);
   }

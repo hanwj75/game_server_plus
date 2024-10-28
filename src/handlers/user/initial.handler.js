272ed30aed944189d7ca7de9bdda3f24204a2e6c
hanwj75 <wjws9608@gmail.com>
2024-10-29 03:09:49 +0900

    Update "initial.handler.js"

--- src/handlers/user/initial.handler.js
+++ src/handlers/user/initial.handler.js
@@ -1,17 +1,26 @@
 import { HANDLER_IDS, RESPONSE_SUCCESS_CODE } from "../../constants/handlerIds.js";
+import { createUser, findUserByDeviceId, updateUserLogin } from "../../db/user/user.db.js";
 import { handlerError } from "../../utils/error/errorHandler.js";
 import { createResponse } from "../../utils/response/createResponse.js";
 
-const initialHandler = ({ socket, userId, payload }) => {
+const initialHandler = async ({ socket, userId, payload }) => {
   try {
     const { deviceId } = payload;
+    //강의 3-10 뭐한건지 모르겠음 나중에다시보셈
+    let user = await findUserByDeviceId(deviceId);
+
+    if (!user) {
+      user = await createUser(deviceId);
+    } else {
+      await updateUserLogin(user.id);
+    }
 
     addUser(socket, deviceId);
 
     const initialResponse = createResponse(
       HANDLER_IDS.INITIAL,
       RESPONSE_SUCCESS_CODE,
-      { userId: deviceId },
+      { userId: user.id },
       deviceId,
     );
 

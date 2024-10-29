0539b481aba4218ec29c9dcf8c1441b2fee4d6f8
hanwj75 <wjws9608@gmail.com>
2024-10-29 23:17:06 +0900

    Update "proto에 명시한갑 추가"

--- src/protobuf/packetNames.js
+++ src/protobuf/packetNames.js
@@ -9,9 +9,14 @@ export const packetNames = {
   game: {
     CreateGamePayload: "game.CreateGamePayload",
     JoinGamePayload: "game.JoinGamePayload",
+    LocationUpdatePayload: "game.LocationUpdatePayload",
   },
   response: {
     Response: "response.Response",
+  },
+  gameNotification: {
+    Start: "gameNotification.Start",
+    LocationUpdate: "gameNotification.LocationUpdate",
   },
 };
 

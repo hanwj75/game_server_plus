55061729b1cf91b079f4a3c774a3ba4f1c36f92b
hanwj75 <wjws9608@gmail.com>
2024-10-29 12:32:09 +0900

    Add "Game클래스 정의"

---
+++ src/classes/models/game.class.js
@@ -0,0 +1,42 @@
+MAX_PLAYERS = 4;
+
+class Game {
+  constructor(id) {
+    this.id = id;
+    this.users = [];
+    this.state = "waiting"; //'waiting'과 'inProgress' 의 2가지 상태값을 가지도록
+  }
+
+  addUser() {
+    if (this.users.length >= MAX_PLAYERS) {
+      throw new Error(`최대인원수임`);
+    }
+    this.users.push(user);
+    //유저의 수가 최대치라면 일정시간후 자동으로 게임이 시작되게 하는 로직
+
+    if (this.users.length === MAX_PLAYERS) {
+      setTimeout(() => {
+        this.startGame();
+      }, 3000);
+    }
+  } // 유저 생성
+
+  getuser(userId) {
+    return this.users.find((user) => user.id === userId);
+  } // 유저 조회
+
+  removeUser(userId) {
+    this.users = this.users.filter((user) => user.id !== userId);
+
+    //게임 시작중 유저가 나가거나 쫒겨날 경우 상태값을 다시 대기중으로 변경
+    if (this.users.length < MAX_PLAYERS) {
+      this.state = "waiting";
+    }
+  } // 유저 삭제
+
+  startGame() {
+    this.state = "inProgress"; //게임이 시작하면 상태값을 게임중으로 바꿈
+  } // 게임 시작
+}
+
+export default Game;

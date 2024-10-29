8f092e90d0598b278e31d4f41d9c871f5c7dcb77
hanwj75 <wjws9608@gmail.com>
2024-10-29 14:49:08 +0900

    Update "게임생성핸들러추가"

--- src/protobuf/packetNames.js
+++ src/protobuf/packetNames.js
@@ -5,7 +5,12 @@ export const packetNames = {
   initial: {
     InitialPacket: "initial.InitialPacket",
   },
+  game: {
+    CreateGamePayload: "game.CreateGamePayload",
+  },
   response: {
     Response: "response.Response",
   },
 };
+
+//proto파일을 만들면 여기에 추가해줘야함
